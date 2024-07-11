cc9551e992696c86389543cb6e436cfe376e1463
UAres213 <albertzhang922@gmail.com>
2024-07-11 15:04:06 +0800

    f

---
+++ ignition/modules/NFT.js
@@ -0,0 +1,5 @@
+const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules"); 
+module.exports = buildModule("NFTModule", (m) => { 
+const nft = m.contract("NFT", []); 
+return { nft }; 
+});
